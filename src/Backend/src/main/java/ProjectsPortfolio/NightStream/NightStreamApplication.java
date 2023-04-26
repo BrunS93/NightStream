bb99c6124ef7ff7b4d2be50b15138d592c092458
BrunS93 <brunal_003@hotmail.com>
2023-04-26 01:10:10 -0300

    initial commit main

---
+++ src/Backend/src/main/java/ProjectsPortfolio/NightStream/NightStreamApplication.java
@@ -0,0 +1,13 @@
+package ProjectsPortfolio.NightStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightStreamApplication.class, args);
+	}
+
+}
---
+++ src/Backend/src/test/java/ProjectsPortfolio/NightStream/NightStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectsPortfolio.NightStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
